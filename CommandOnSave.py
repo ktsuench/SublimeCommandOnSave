54a9877b76342401e859b226e8777c0f812a9ae7
will289 <will289@users.noreply.github.com>
2015-07-09 03:40:28 -0400

    Update CommandOnSave.py

--- CommandOnSave.py
+++ CommandOnSave.py
@@ -15,6 +15,6 @@ class CommandOnSave(sublime_plugin.EventListener):
                 if file.startswith(path) and len(commands) > 0:
                     print("Command on Save:")
                     for command in commands:
-                        p = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
+                        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
                         out, err = p.communicate()
                         print (out.decode('utf-8'))
