fef7fab36a06fb22d69e62a27dbe65c8b223834d
will289 <will289@users.noreply.github.com>
2015-08-16 20:05:04 -0400

    Add file name as argument
    Use regular expressions to retrieve file name from file path and send it as an argument

--- CommandOnSave.py
+++ CommandOnSave.py
@@ -1,20 +1,21 @@
 import sublime
 import sublime_plugin
 import subprocess
-
+import re
 
 class CommandOnSave(sublime_plugin.EventListener):
     def on_post_save(self, view):
 
         settings = sublime.load_settings('CommandOnSave.sublime-settings').get('commands')
-        file = view.file_name()
-
+        filepath = view.file_name()
+        file = re.search('[^\/]*$').group(0)
+        
         if not settings == None:
             for path in settings.keys():
                 commands = settings.get(path)
-                if file.startswith(path) and len(commands) > 0:
+                if filepath.startswith(path) and len(commands) > 0:
                     print("Command on Save:")
                     for command in commands:
-                        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+                        p = subprocess.Popen([command, file], shell=True, stdout=subprocess.PIPE)
                         out, err = p.communicate()
                         print (out.decode('utf-8'))
